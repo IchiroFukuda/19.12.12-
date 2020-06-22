06257b0eb9329c07ace8e8cea2edec01f939a8e8
IchiroFukuda <ichr0113@gmail.com>
2020-06-22 10:43:09 +0900

    firebase導入

--- 19.12.12禁欲モンキー/AppDelegate.swift
+++ 19.12.12禁欲モンキー/AppDelegate.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
         
         let userDefault = UserDefaults.standard
         if userDefault.object(forKey: "onakinSwitch") != nil {
